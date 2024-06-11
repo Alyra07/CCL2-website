0bc70ccb345cb58b1e6c3834e993a68bccb467c8
Alyra07 <96477039+Alyra07@users.noreply.github.com>
2024-06-11 21:17:34 +0200

    fancy profile pics

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { supabase } from '../supabaseClient';
 import { useUser } from '../UserContext';
+import '../index.css';
 
 const Profile = () => {
   const { user } = useUser();
   const [profile, setProfile] = useState(null);
   const [message, setMessage] = useState('');
+  const [selectedImage, setSelectedImage] = useState('');
+  const imagePaths = [
+    '/img/profile-img1.png',
+    '/img/profile-img2.png',
+    '/img/profile-img3.png',
+    '/img/profile-img4.png',
+    '/img/profile-img-cat.png',
+    '/img/profile-img-rabbit.png',
+    '/img/profile-img-meme.jpeg',
+  ];
 
   useEffect(() => {
     if (user) {
@@ -29,6 +40,25 @@ const Profile = () => {
     }
   };
 
+  const handleImageChange = async () => {
+    try {
+      // Update user profile with selected image
+      const { data, error } = await supabase
+        .from('users')
+        .update({ image: selectedImage })
+        .eq('email', user.email);
+
+      if (error) {
+        throw error;
+      }
+
+      setMessage('Profile image updated successfully');
+    } catch (error) {
+      console.error('Error updating profile image:', error.message);
+      setMessage('Error updating profile image.');
+    }
+  };
+
   if (!profile) {
     return <div>Loading...</div>;
   }
@@ -37,6 +67,19 @@ const Profile = () => {
     <div>
       <h2>Profile</h2>
       <p>{message}</p>
+      <div>
+          <p>Profile Picture:</p>
+          <img src={profile.image} alt="Profile" style={{ width: '300px', height: '300px' }} />
+        
+        <label>Select Profile Picture:</label>
+        <select value={selectedImage} onChange={(e) => setSelectedImage(e.target.value)}>
+          <option value="">Select an image</option>
+          {imagePaths.map((path, index) => (
+            <option key={index} value={path}>{index + 1}</option>
+          ))}
+        </select>
+        <button onClick={handleImageChange}>Set Profile Picture</button>
+      </div>
       <div>
         <p>Email: {profile.email}</p>
         <p>Name: {profile.name}</p>
