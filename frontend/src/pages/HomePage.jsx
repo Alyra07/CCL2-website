6cbbea1c6818bcf3d95c52bef6e4f7c6de0b1e14
Alyra07 <96477039+Alyra07@users.noreply.github.com>
2024-06-17 00:06:51 +0200

    HomePage bg

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -52,45 +52,54 @@ const HomePage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <header className="bg-offwhite pt-8 px-4 md:px-10 lg:px-16 text-center w-full">
-        {/* search bar field with heading */}
-        <div className='flex flex-col text-center mt-8'> 
-        <h2 className="text-4xl font-semibold text-primary my-10">
-          Hello World! This is a travel website.
-        </h2>
-        <SearchBar onSearch={handleSearch} countries={countries} />
-        </div>
-      </header>
-      <section className="w-full max-w-screen-lg mt-2 mb-8 mx-auto py-8 px-4">
-        <h2 className="text-2xl font-semibold mb-6 text-center">Popular Places</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {popularCountries.map(({ country }) => (
-            <div key={country} className="bg-white shadow-md rounded-lg overflow-hidden">
-              <div className="relative">
-              <img 
-                src={`/img/${country}.jpg`} 
-                alt={country} 
-                className="w-full h-48 object-cover" 
-                onError={(e) => e.target.src = '/img/placeholder.jpg'}
-              />
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                  <h3 className="text-2xl text-white font-semibold">{country}</h3>
+    <div className="flex flex-col justify-between min-h-screen bg-white shadow-md overflow-hidden">
+      <div>
+        <header className="text-center w-full">
+          <div className="relative">
+            <img 
+              src="/img/hero.jpg" 
+              alt="Travel" 
+              className="w-full h-64 object-cover" 
+              onError={(e) => e.target.src = '/img/bg-bedroom.jpg'}
+            />
+            <div className="flex flex-col absolute inset-0 bg-black bg-opacity-50 items-center justify-center">
+              <h1 className="flex flex-col text-center mt-8 text-4xl font-semibold text-white py-10">
+                Hello World! This is a travel website.
+              </h1>
+              <SearchBar onSearch={handleSearch} countries={countries} />
+            </div>
+          </div>
+        </header>
+        <section className="w-full max-w-screen-lg mt-2 mb-8 mx-auto py-8 px-4">
+          <h2 className="text-2xl font-semibold mb-6 text-center">Popular Places</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {popularCountries.map(({ country }) => (
+              <div key={country} className="bg-white shadow-md rounded-lg overflow-hidden">
+                <div className="relative">
+                  <img 
+                    src={`/img/${country}.jpg`} 
+                    alt={country} 
+                    className="w-full h-48 object-cover" 
+                    onError={(e) => e.target.src = '/img/placeholder.jpg'}
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <h3 className="text-2xl text-white font-semibold">{country}</h3>
+                  </div>
+                </div>
+                <div className="p-6">
+                  <p className="text-gray-700 mb-2">Discover the best places to stay in {country}.</p>
+                  <button
+                    onClick={() => navigate('/list', { state: { searchCriteria: { country }, filteredListings: [] } })}
+                    className="bg-primary text-white py-2 px-4 rounded hover:bg-secondary transition-colors duration-300"
+                  >
+                    Explore {country}
+                  </button>
                 </div>
               </div>
-              <div className="p-6">
-                <p className="text-gray-700 mb-2">Discover the best places to stay in {country}.</p>
-                <button
-                  onClick={() => navigate('/list', { state: { searchCriteria: { country }, filteredListings: [] } })}
-                  className="bg-primary text-white py-2 px-4 rounded hover:bg-secondary transition-colors duration-300"
-                >
-                  Explore {country}
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
+      </div>
       <Footer />
     </div>
   );
