81ffd48c9a888c5077fa3064cdaac7a6ca53c9f3
Alyra07 <96477039+Alyra07@users.noreply.github.com>
2024-06-17 13:39:33 +0200

    Profile css

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
 import { useUser } from '../assets/UserContext.jsx';
 import '../index.css';
 
 const Profile = () => {
   const { user } = useUser();
+  const navigate = useNavigate();
   const [profile, setProfile] = useState(null);
   const [listings, setListings] = useState([]);
   const [message, setMessage] = useState('');
@@ -68,7 +69,12 @@ const Profile = () => {
       setMessage('Error fetching listings.');
     }
   };
-  
+
+  const handleListingClick = (id) => {
+    // Pass listing id to DetailsListing page
+    navigate(`/profile/listing/${id}`);
+  };
+
   const handleImageChange = async () => {
     // Update profile image in the database
     try {
@@ -128,6 +134,133 @@ const Profile = () => {
       setMessage('Error updating profile surname.');
     }
   };
+
+  // default return if user is logged in
+  return (
+    <div className="p-4 md:p-10 lg:p-16">
+      <div className="text-center">
+        <h2 className="font-semibold text-primary text-3xl">
+          Profile
+        </h2>
+        <p className="text-green-500">{message}</p>
+      </div>
+      {/* Profile data display */}
+      {profile && (
+        <div className="rounded-lg bg-tertiary p-6 mx-auto w-full md:w-2/3 lg:w-1/2">
+          {/* Profile image */}
+          <div className="text-center mb-4">
+            <img src={profile.image} alt="Profile"
+              className="w-64 h-auto border border-secondary rounded-lg mx-auto mb-4" />
+            <label className="block mb-2">Select Profile Picture:</label>
+            <select
+              value={selectedImage}
+              onChange={(e) => setSelectedImage(e.target.value)}
+              className="border p-2 rounded mb-4"
+            >
+              <option value="">Select an image</option>
+              {imagePaths.map((path, index) => (
+                <option key={index} value={path}>{`Image ${index + 1}`}</option>
+              ))}
+            </select>
+            <button
+              onClick={handleImageChange}
+              className="bg-primary text-white p-2 ml-2 rounded-lg hover:bg-secondary transition duration-300"
+            >
+              Set Profile Picture
+            </button>
+          </div>
+
+          {/* User Data Display with Edit */}
+          <div className="flex justify-center items-center mb-4">
+            <div className="flex flex-col">
+              <div className="justify-center">
+                <p className="text-lg font-semibold">Email: {profile.email}</p>
+                <p className="text-lg font-semibold">
+                  Name: {isEditingName ? (
+                    <input
+                      type="text"
+                      value={newName}
+                      onChange={(e) => setNewName(e.target.value)}
+                      className="border p-2 rounded"
+                    />
+                  ) : (
+                    profile.name
+                  )}
+                  <button
+                    onClick={() => (isEditingName ? handleNameChange() : setIsEditingName(true))}
+                    className="mx-4 my-2 bg-accent text-base text-white py-1 px-2 rounded-lg hover:bg-red-300"
+                  >
+                    {isEditingName ? 'Save' : 'Edit'}
+                  </button>
+                </p>
+                <p className="text-lg font-semibold">
+                  Surname: {isEditingSurname ? (
+                    <input
+                      type="text"
+                      value={newSurname}
+                      onChange={(e) => setNewSurname(e.target.value)}
+                      className="border p-2 rounded"
+                    />
+                  ) : (
+                    profile.surname
+                  )}
+                  <button
+                    onClick={() => (isEditingSurname ? handleSurnameChange() : setIsEditingSurname(true))}
+                    className="mx-4 bg-accent text-base text-white py-1 px-2 rounded-lg hover:bg-red-300"
+                  >
+                    {isEditingSurname ? 'Save' : 'Edit'}
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* User Listings "Your Places" */}
+          <div className="mb-2 text-center">
+            <h2 className="font-bold text-2xl my-6">Your Places</h2>
+            {Array.isArray(listings) && listings.length > 0 ? (
+              <div className="flex justify-center">
+                <div className="grid grid-cols-2 xl:grid-cols-3 gap-8">
+                  {listings.map((listing) => (
+                    <div
+                      key={listing.id}
+                      className="w-48 bg-light-gray rounded-lg overflow-hidden shadow-lg 
+                                transform transition-transform duration-300 hover:scale-105"
+                      onClick={() => handleListingClick(listing.id)}
+                    >
+                      <img
+                        src={listing.images?.[0] || '/img/placeholder2.jpg'}
+                        alt={listing.name}
+                        className="w-full h-52 object-cover"
+                      />
+                      <div className="p-4">
+                        <h3 className="text-xl font-semibold mb-2">{listing.name}</h3>
+                        <p className="text-gray-700">{listing.address}</p>
+                        <p className="text-gray-700">{listing.country}</p>
+                        <p className="text-gray-700">${listing.price} per night</p>
+                        <p className="text-gray-700">{listing.guests} guests</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <p className="text-center">You have no listings yet...</p>
+            )}
+          </div>
+        </div>
+      )}
+      <div className="my-4 text-center">
+        <Link
+          to="/add"
+          className="bg-accent text-white py-2 px-4 rounded-lg hover:bg-red-300 transition duration-300"
+        >
+          Add a listing
+        </Link>
+      </div>
+    </div>
+  );
+
   // if not logged in, display message to sign in
   if (!profile) {
     return (
@@ -140,128 +273,6 @@ const Profile = () => {
       </div>
     );
   }
-
-  return (
-    <div className="p-4 md:p-10 lg:p-16">
-      <div className="text-center">
-      <h2 className="font-semibold text-primary text-3xl mb-4">Profile</h2>
-      <p className="text-green-500">{message}</p>
-      {/* Profile Picture */}
-      <div className="mb-4">
-        <img src={profile.image} alt="Profile" 
-        className="w-64 h-64 border-2 border-tertiary rounded-lg mx-auto mb-4" />
-
-        <label className="block mb-2">Select Profile Picture:</label>
-        <select
-          value={selectedImage}
-          onChange={(e) => setSelectedImage(e.target.value)}
-          className="border p-2 rounded mb-4"
-        >
-          <option value="">Select an image</option>
-          {imagePaths.map((path, index) => (
-            <option key={index} value={path}>{`Image ${index + 1}`}</option>
-          ))}
-        </select>
-        <button
-          onClick={handleImageChange}
-          className="bg-primary text-white p-2 ml-2 rounded-lg hover:bg-secondary transition duration-300"
-        >
-          Set Profile Picture
-        </button>
-      </div>
-      </div>
-
-      {/* User Data Display with Edit */}
-      <div className="flex justify-center items-center">
-      <div className="flex flex-col mb-4">
-        <div className="justify-center">
-          <p className="text-lg font-semibold">Email: {profile.email}</p>
-          <p className="text-lg font-semibold">
-            Name: {isEditingName ? (
-              <input
-                type="text"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-                className="border p-2 rounded"
-              />
-            ) : (
-              profile.name
-            )}
-            <button
-              onClick={() => (isEditingName ? handleNameChange() : setIsEditingName(true))}
-              className="mx-4 my-2 bg-accent text-base text-white py-1 px-2 rounded-lg hover:bg-red-300"
-            >
-              {isEditingName ? 'Save' : 'Edit'}
-            </button>
-          </p>
-          <p className="text-lg font-semibold">
-            Surname: {isEditingSurname ? (
-              <input
-                type="text"
-                value={newSurname}
-                onChange={(e) => setNewSurname(e.target.value)}
-                className="border p-2 rounded"
-              />
-            ) : (
-              profile.surname
-            )}
-            <button
-              onClick={() => (isEditingSurname ? handleSurnameChange() : setIsEditingSurname(true))}
-              className="mx-4 bg-accent text-base text-white py-1 px-2 rounded-lg hover:bg-red-300"
-            >
-              {isEditingSurname ? 'Save' : 'Edit'}
-            </button>
-          </p>
-        </div>
-      </div>
-    </div>
-      {/* User Listings */}
-      <div className="mb-4 text-center">
-        <h2 className="font-bold text-2xl mb-4">Your Places</h2>
-        {Array.isArray(listings) && listings.length > 0 ? (
-          <div className="flex justify-center">
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-              {listings.map((listing) => (
-                <div
-                  key={listing.id}
-                  className="border border-gray-300 rounded-lg overflow-hidden shadow-lg"
-                >
-                  <img
-                    src={listing.images?.[0] || '/img/placeholder2.jpg'}
-                    alt={listing.name}
-                    className="w-full h-52 object-cover"
-                  />
-                  <div className="p-4">
-                    <h3 className="text-xl font-semibold mb-2">{listing.name}</h3>
-                    <p className="text-gray-700">{listing.address}</p>
-                    <p className="text-gray-700">{listing.country}</p>
-                    <p className="text-gray-700">${listing.price} per night</p>
-                    <p className="text-gray-700">{listing.guests} guests</p>
-                    <Link
-                      to={`/profile/listing/${listing.id}`}
-                      className="text-accent hover:underline"
-                    >
-                      View Details
-                    </Link>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <p className="text-center">You have no listings yet...</p>
-        )}
-        <div className="mt-6 md:mt-8 text-center">
-          <Link
-            to="/add"
-            className="bg-primary text-white py-2 px-4 rounded-lg hover:bg-secondary transition duration-300"
-          >
-            Add a listing
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default Profile;
