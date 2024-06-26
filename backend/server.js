d0bb14cc183c9be4475bb54422dc3c8f42de7c12
Alyra07 <96477039+Alyra07@users.noreply.github.com>
2024-06-26 19:18:15 +0200

    dependency cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
